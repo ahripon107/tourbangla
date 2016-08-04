33be5dcedf7b6c7f08717617516806674b96bbe6
Aminul Hoque <ah.ripon10@gmail.com>
2016-08-04 07:52:04 +0600

    some bugs fixed

--- app/src/main/java/com/androidfragmant/tourxyz/banglatourism/fragment/HomeFragment.java
+++ app/src/main/java/com/androidfragmant/tourxyz/banglatourism/fragment/HomeFragment.java
@@ -31,7 +31,7 @@ public class HomeFragment extends Fragment {
         elements = new ArrayList<>();
         elements.add(new HomeFragmentElement("Explore amazing tourist spots of beautiful Bangladesh!","Explore Places"));
         elements.add(new HomeFragmentElement("Explore offers from tour operators and hotels!","Tour offers"));
-        elements.add(new HomeFragmentElement("Want to share your's and read other's tour experience? Explore Tour Blog!","Tour Blog"));
+        elements.add(new HomeFragmentElement("Want to share your and read other's tour experience? Explore Tour Blog!","Tour Blog"));
         elements.add(new HomeFragmentElement("Want to know anything about a place? Ask in Forum!","Forum"));
     }
 
@@ -48,22 +48,6 @@ public class HomeFragment extends Fragment {
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
 
-        LikeView likeView = (LikeView) rootView.findViewById(R.id.likeView);
-        likeView.setLikeViewStyle(LikeView.Style.STANDARD);
-        likeView.setAuxiliaryViewPosition(LikeView.AuxiliaryViewPosition.INLINE);
-
-        likeView.setObjectIdAndType(
-                "https://www.facebook.com/Civil10-Premier-League-CPL-1413055355581750/",
-                LikeView.ObjectType.PAGE);
-
-        ShareButton shareButton = (ShareButton) rootView.findViewById(R.id.shareButton);
-        String appPackageName = getContext().getPackageName();
-        ShareLinkContent linkContent;
-        String aURL = "https://play.google.com/store/apps/details?id=" + appPackageName;
-        linkContent = new ShareLinkContent.Builder()
-                .setContentUrl(Uri.parse(aURL))
-                .build();
-        shareButton.setShareContent(linkContent);
         return rootView;
     }
 
